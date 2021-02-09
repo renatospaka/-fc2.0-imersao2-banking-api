5f2ae1071f3e03644e86afd537583077bfc8e93b
Renato Spakauskas <renatospaka@gmail.com>
2021-02-08 23:16:22 -0300

    feat: :construction: bank accounts - wip

--- src/migrations/1612826477307-CreateBankAccountsTable.ts
+++ src/migrations/1612826477307-CreateBankAccountsTable.ts
@@ -1,11 +1,39 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateBankAccountsTable1612826477307 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'bank_accounts',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'account_number',
+            type: 'varchar'
+          },
+          {
+            name: 'owner_name',
+            type: 'varchar'
+          },
+          {
+            name: 'balance',
+            type: 'double precision'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP'
+          }
+        ]
+      })
+    )
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('bank_accounts')
+  }
 }
