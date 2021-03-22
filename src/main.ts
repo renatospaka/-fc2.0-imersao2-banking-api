231630eca192472009671449ff29ffdd614ace9d
Renato Spakauskas <renatospaka@gmail.com>
2021-03-22 15:01:36 -0300

    feat: :sparkles: bank accounts - finished

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ModelNotFoundExceptionFilter } from './exceptionFilters/modelNotFound.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new ModelNotFoundExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
